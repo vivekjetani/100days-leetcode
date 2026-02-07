ac5d604408a27cb9a16ff7cb6a74aaeed5dcd207
jetani vivek <119884882+vivekjetani@users.noreply.github.com>
2026-02-07 19:28:08 +0530

    add 1653 MinimumDeletionstoMakeStringBalanced.java

---
+++ 1653 MinimumDeletionstoMakeStringBalanced.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        int bCount = 0;  
+        int deletions = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == 'b') {
+                bCount++;
+            } else {
+                deletions = Math.min(deletions + 1, bCount);
+            }
+        }
+        return deletions;
+    }
+}
